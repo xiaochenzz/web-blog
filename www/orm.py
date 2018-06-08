d18cc9b301eb7c1ffbca2cccc0ea242861c2242e
xiaochenzz <zhongjvzhang@gmail.com>
2018-06-09 00:28:57 +0800

    orm

---
+++ www/orm.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/8 23:36
+# @Author  : zzj
+# @Email   : zhongjv@163.com
+# @File    : orm.py
+# @Software: PyCharm
+
+
+import asyncio
+import logging
+import aiomysql
+
+
+def log(sql, args=()):
+	logging.info('SQL: %s' % sql)
+
+
+async def create_pool(loop, **kw):
+	logging.info('create database connection pool...')
+	global __pool
+	__pool = await aiomysql.create_pool(
+		host=kw.get('host', 'localhost'),
+		port=kw.get('port', 3306),
+		user=kw['user'],
+		password=kw['password'],
+		db=kw['db'],
+		charset=kw.get('charset', 'utf8'),
+		autocommit=kw.get('autocommit', True),
+		maxsize=kw.get('maxsize', 10),
+		minsize=kw.get('minsize', 1),
+		loop=loop
+	)
+
+
+async def select(sql, args, size=None):
+	log(sql, args)
+	global __pool
+	async with __pool.get() as conn:
+		async with conn.cursor(aiomysql.DictCursor) as cur:
+			await cur.execute(sql.replace('?', '%s'), args or ())
+			if size:
+				rs = await cur.fetchmany()
+			else:
+				rs = await cur.fetchall()
+		logging.info('rows returned: %s' % len(rs))
+		return rs
+
+
+async def execute(sql, args, autocommit=True):
+	log(sql)
+	async with __pool.get() as conn:
+		if not autocommit:
+			await conn.begin()
+		try:
+			async with conn.cursor(aiomysql.DictCursor) as cur:
+				await cur.execute(sql.replace('?', '%s'), args)
+				affected = cur.rowcount
+			if not autocommit:
+				await conn.commit()
+		except BaseException as e:
+			if not autocommit:
+				await conn.rollback()
+			raise
+		return affected
+
+
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ','.join(L)
+
+
+class Field(object):
+
+	def __init__(self, name, column_type, primary_key, default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type,
+		                        self.name)
+
+
+class StringField(Field):
+
+	def __init__(self, name=None, primary_key=False, default=None,
+	             ddl='varchar(100'):
+		super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+	def __init__(self, name=None, default=False):
+		super().__init__(name, 'boolean', False, default)
