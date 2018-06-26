2a80f6d278d2fb7ea1c724d02189ea1cf7a67389
zj <zhongjvzhang@gmail.com>
2018-06-26 17:26:28 +0800

    CSS

--- www/handlers.py
+++ www/handlers.py
@@ -8,15 +8,37 @@
 
 'url handlers'
 
-from coroweb import  get
+
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
 import asyncio
+
+from coroweb import  get,post
+from models import User, Comment, Blog, next_id
 
 
 @get('/')
-async def index(request):
-	return '<h1>Awesome</h1>'
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    #for u in users:
+       # u.passwd = '******'
+    return dict(users=users)
 
 
-@get('/hello')
-async def hello(request):
-	return '<h1>hello!</h1>'
